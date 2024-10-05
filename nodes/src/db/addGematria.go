69e4621c094869edcd7dccbd341496baa53e0f28
ranon rat <66473662+ranon-rat@users.noreply.github.com>
2024-10-05 10:07:47 -0600

    Update addGematria.go

--- nodes/src/db/addGematria.go
+++ nodes/src/db/addGematria.go
@@ -1,19 +1,20 @@
 package db
 
 import (
-	"fmt"
 	"time"
 )
 
 // so this will store the gematria in the databsae, its not that complicated obviously xd
-func AddGematria(input, format string) {
+// if there is an error obviously its because the value its repeated
+func AddGematria(input, format string) (err error) {
 	query := `INSERT INTO gematrias(input,search,datePost) VALUES(?1,?2,?3)`
 	db := Connect()
 	defer db.Close()
-	_, err := db.Exec(query, input, format, int(time.Now().Unix()))
+	_, err = db.Exec(query, input, format, int(time.Now().Unix()))
 	if err != nil {
-		fmt.Println(err)
+		return
 	}
 	// no deberia de ser muy complicado
 	// ahora voy a intentar correr una cosa para hacer el setup
+	return
 }
